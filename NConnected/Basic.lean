366f4f28c463309ca05330ff00f86d030d3c254d
SHW715 <ShuhanWang413@outlook.com>
2026-02-05 17:22:30 +0100

    add hfibre and n-connected map

--- NConnected/Basic.lean
+++ NConnected/Basic.lean
@@ -15,7 +15,6 @@ class NConnected (n : ℕ) : Prop where
   trivial_homotopy (x : X) {k : ℕ} (hk : k ≤ n) : Subsingleton (HomotopyGroup.Pi k X x)
 /- drop nonempty-/
 
-namespace NConnected
 
 /- the topological space is 0-connected iff it is connected. -/
 theorem nConnected_zero_iff_pathConnectedSpace :
@@ -75,3 +74,85 @@ theorem nConnected_one_iff_simplyConnectedSpace :
         rw [hk₁]
         haveI : Subsingleton (FundamentalGroup X x) := h' x x
         exact Equiv.subsingleton (HomotopyGroup.pi1EquivFundamentalGroup)
+
+/- Our next goal is to define `n`-connected map. To define this, we shall first
+define `homotopy fibre`. -/
+
+universe u v
+variable {X : Type u} {Y : Type v} [TopologicalSpace X] [TopologicalSpace Y]
+
+/-- Homotopy fiber of `f` over `y`. -/
+def hFiber (f : C(X, Y)) (y : Y) : Type (max u v) :=
+  Σ x : X, Path (f x) y
+
+-- We shall first endow homotopy fibre a topological structure:
+instance (f : C(X, Y)) (y : Y) : TopologicalSpace (hFiber f y) := by
+  unfold hFiber; infer_instance
+
+
+/-- `f` is `n`-connected if all homotopy fibers are `(n-1)`-connected spaces. -/
+def NConnectedMap (f : C(X, Y)) (n : ℕ) : Prop :=
+  ∀ y : Y,
+    match n with
+    | 0     => Nonempty (hFiber f y)      -- 0-connected = nonempty fiber
+    | n+1   => NConnected (hFiber f y) n  -- (n+1)-connected map → n-connected fiber
+
+/- The definition of `f : X → Y` being a `n`-connected map is given by:
+1. 0-connected if `π_0 f : π_0 (X) → π_0 (Y)` is surjective;
+2. n-connected if `π_0 f : π_0 (X) → π_0 (Y)` is bijective and for any basepoint,
+`π_i (f,x) : π_i (X,x) → π_i (Y,x)` are bijective for `1≤ i≤ n-1` and surjective for `i=n`.
+
+  However, we don't have proper definition of `π_i f` in mathlib. Therefore, we adopted above
+definition. Now we'd like to check compatibility of two definition. Due to lack of proper
+definition of `π_i f`, we need to first define it (we only check `n=0` case, as for `n≥ 1`
+case, it will involve LES of fibration.
+-/
+
+
+def ZerothHomotopy.map (f : C(X, Y)) : ZerothHomotopy X → ZerothHomotopy Y :=
+by
+  refine Quotient.map (fun x : X => (f x)) ?_
+  intro a b hab
+  obtain ⟨ p ⟩ := hab
+  exact ⟨p.map f.continuous⟩
+
+
+
+/- Now we'd like to show that `f is 0-connected map` defined by us is equivalent to `π_0 f`
+surjective. -/
+
+theorem zeroConnected_surjective_ZH (f : C(X, Y))
+  (h0 : NConnectedMap f 0) :
+  Function.Surjective (ZerothHomotopy.map f) :=
+by
+   intro cy
+   refine Quotient.inductionOn cy ?_
+   intro y
+   obtain ⟨x, p⟩ := h0 y
+   refine ⟨Quotient.mk (pathSetoid X) x, ?_⟩
+   exact Quotient.sound ⟨p⟩
+
+
+theorem surjective_ZH_zeroConnected (f : C(X, Y))
+  (hs : Function.Surjective (ZerothHomotopy.map f)) :
+  NConnectedMap f 0 :=
+by
+  intro y
+  rcases hs (Quotient.mk _ y) with ⟨cx, hcxy⟩
+  refine Quotient.inductionOn cx ?_ hcxy
+  intro x hxy
+  have hj : Joined (f x) y := by
+    have : (Quotient.mk _ (f x) : ZerothHomotopy Y) = Quotient.mk _ y := by
+      simpa [ZerothHomotopy.map] using hxy
+    exact Quotient.exact this
+  rcases hj with ⟨p⟩
+  exact ⟨⟨x, p⟩⟩
+
+/-- `f` is 0-connected if and only if `π_0 f` is surjective -/
+theorem zeroConnected_iff_surjective_ZH (f : C(X, Y)) :
+  NConnectedMap f 0
+    ↔ Function.Surjective (ZerothHomotopy.map f) :=
+by
+  constructor
+  · intro h0; exact zeroConnected_surjective_ZH f h0
+  · intro hs; exact surjective_ZH_zeroConnected f hs
